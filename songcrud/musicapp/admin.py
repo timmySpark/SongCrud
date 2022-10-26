517356f2211fecd6d767b7194587e22bb8963b7e
timmySpark <timmyspark1@gmail.com>
2022-10-26 19:59:58 +0100

    Refactor(admin.py): Registered  Artiste , Song and Lyric models

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from musicapp.models import *
 
 # Register your models here.
+
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
+
