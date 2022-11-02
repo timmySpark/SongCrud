34488a982cf49b59df13ff14045d6fc2571214be
timmySpark <timmyspark1@gmail.com>
2022-11-02 09:14:25 +0000

    Refactor(musicapp):added serializers.py file to  musicapp

---
+++ songcrud/musicapp/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import *
+
+class ArtisteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artiste
+        fields = ['first_name','last_name','age']
+
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ['artiste_id','title','date_released','likes']
+
+
+class LyricSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lyric
+        fields = ['song_id','content']
+
