c9f012da609698f492111fd2b3617ef5782f3c34
timmySpark <timmyspark1@gmail.com>
2022-11-02 09:17:01 +0000

    Refactor(urls):added new urls for views

--- songcrud/musicapp/urls.py
+++ songcrud/musicapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('artistes', artiste_list),
+    path('songs', song_list),
+    path('song/<int:id>', song_detail),
+    # path('lyrics', lyric_list),
 ]
